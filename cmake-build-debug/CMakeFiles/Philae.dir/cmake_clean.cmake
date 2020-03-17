d2aa26820e7c462599c072c4852a520f561bf391
cyrilleio <52087324+cyrilleio@users.noreply.github.com>
2020-03-17 11:14:24 +0100

    Clion project

---
+++ cmake-build-debug/CMakeFiles/Philae.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Philae.dir/main.c.o"
+  "Philae"
+  "Philae.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Philae.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
